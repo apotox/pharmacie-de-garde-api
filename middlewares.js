d97a88f94734eb31b980120e4978cb7123523526
saphidev <saphidev@gmail.com>
2021-08-07 10:16:11 +0100

    return array instead of object

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,4 @@
-const moment = require("moment");
+const moment = require("moment-timezone");
 
 const validateBody = (req, res, next)=>{
   const id = req.params.id;
@@ -13,22 +13,28 @@ const validateBody = (req, res, next)=>{
       message: "body.weekGarde must be an array len <= 30"});
   }
 
-  const today = moment().tz("Africa/Algiers");
+  if (data.weekGarde.filter( (item) => !Array.isArray(item)).length != 0) {
+    return res.status(400).json({success: false,
+      message: "body.weekGarde.item must be an array"});
+  }
 
-  const payload = data.weekGarde.sort((a, b)=>b.index > a.index)
-      .map((item, index)=>{
-        const nextday = today.add(index, "day");
-        return {
+  const today = moment().tz("Africa/Algiers");
+  // .sort((a, b)=>b.index > a.index)
+  const payload = data.weekGarde
+      .reduce((obj, dayGardes=[], index)=>{
+        const nextday = today.add(index, "day").format("yyyy-MM-DD");
+        obj[nextday]= dayGardes.map((item)=>({
           name: item.name,
-          date: nextday.format("yyyy-MM-dd"),
+          date: nextday,
           hourFrom: item.hourFrom,
           hourTo: item.hourTo,
           location: `${item.lat},${item.lng}`,
           rating: 5,
-        };
-      });
+        }));
+        return obj;
+      }, {});
 
-  res.locals.data = payload;
+  res.locals.payload = payload;
   return next();
 };
 
