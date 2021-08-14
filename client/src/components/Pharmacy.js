9076aba39f2635e67413799a8fc551dcc633daa0
saphidev <saphidev@gmail.com>
2021-08-14 16:20:47 +0100

    deleteGarde option

--- client/src/components/Pharmacy.js
+++ client/src/components/Pharmacy.js
@@ -4,15 +4,23 @@ import {
     CardTitle, CardSubtitle
 } from 'reactstrap';
 import {FaTrash} from 'react-icons/fa'
+import { useDispatch } from 'react-redux';
+import { DELETE_GARDES_BY_INDEX } from '../redux/actions/app.actions';
 
-function Pharmacy({pharmacy}) {
+function Pharmacy({day,pharmacy,index}) {
+
+    const dispatch = useDispatch()
+
+    const deleteGarde=()=>{
+        dispatch(DELETE_GARDES_BY_INDEX(day,index))
+    }
     return (
         <Card onClick={e=> e.stopPropagation()} >
             <CardBody>
                 <CardTitle tag="h5">{pharmacy.name}</CardTitle>
                 <CardSubtitle tag="h6" className="mb-2 text-muted">{`${pharmacy.hourFrom} - ${pharmacy.hourTo}`}</CardSubtitle>
 
-                <div className='svg-container'>
+                <div className='svg-container' onClick={deleteGarde}>
                     <FaTrash />
                 </div>
             </CardBody>
