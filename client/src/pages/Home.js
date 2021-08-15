ae0f48b2547e71c558598d10a3eb82778a97db15
saphidev <saphidev@gmail.com>
2021-08-15 17:38:07 +0100

    fix home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,18 +2,42 @@ import React from 'react'
 // import Footer from '../components/Footer'
 import '../glitch.css'
 
-
+import { useHistory } from 'react-router-dom'
 function Home() {
+
+    const history = useHistory()
     return (
-        <div>
-            <h3>welcome to home</h3>
+        <div className='home-page'>
 
             <div className='cybr-btn-container'>
-            <button class="cybr-btn">
-                Safi<span aria-hidden>_</span>
-                <span aria-hidden class="cybr-btn__glitch">Apotox_</span>
-                <span aria-hidden class="cybr-btn__tag">BATNA05</span>
-            </button>
+                <button class="cybr-btn" onClick={() => {
+                    history.push('/dashboard')
+                }}>
+                    Dashboard<span aria-hidden>_</span>
+                    <span aria-hidden class="cybr-btn__glitch">Apotox_</span>
+                    <span aria-hidden class="cybr-btn__tag">BATNA05</span>
+                </button>
+            </div>
+
+
+            <div className='cybr-btn-container'>
+                <button class="cybr-btn" onClick={() => {
+                    window.location.href='https://github.com/apotox/pharmacie-de-garde-api'
+                }}>
+                    Github<span aria-hidden>_</span>
+                    <span aria-hidden class="cybr-btn__glitch">Apotox_</span>
+                    <span aria-hidden class="cybr-btn__tag">BATNA05</span>
+                </button>
+            </div>
+
+            <div className='cybr-btn-container'>
+                <button class="cybr-btn" onClick={() => {
+                    window.location.href='https://twitter.com/saphidev'
+                }}>
+                    Twitter<span aria-hidden>_</span>
+                    <span aria-hidden class="cybr-btn__glitch">Apotox_</span>
+                    <span aria-hidden class="cybr-btn__tag">BATNA05</span>
+                </button>
             </div>
         </div>
     )
