52d4dda965bb2766bfa590541e5f1ae6ea404aa0
saphidev <saphidev@gmail.com>
2021-08-16 18:11:39 +0100

    update home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,53 +3,44 @@ import React from 'react'
 import '../glitch.css'
 
 import { useHistory } from 'react-router-dom'
+import { Button } from 'reactstrap'
+import {FaGithub, FaTwitter} from 'react-icons/fa'
 function Home() {
 
     const history = useHistory()
     return (
         <div className='home-page'>
 
-            <div className='cybr-btn-container'>
-                <button class="cybr-btn" onClick={() => {
+                <Button class="cybr-btn" onClick={() => {
                     history.push('/dashboard')
                 }}>
-                    Dashboard<span aria-hidden>_</span>
-                    <span aria-hidden class="cybr-btn__glitch">Apotox_</span>
-                    <span aria-hidden class="cybr-btn__tag">BATNA05</span>
-                </button>
-            </div>
+                    Dashboard
+                </Button>
 
 
-            <div className='cybr-btn-container'>
-                <button class="cybr-btn" onClick={() => {
+                <Button class="cybr-btn" onClick={() => {
                     window.location.href='https://github.com/apotox/pharmacie-de-garde-api'
                 }}>
-                    Github<span aria-hidden>_</span>
-                    <span aria-hidden class="cybr-btn__glitch">Apotox_</span>
-                    <span aria-hidden class="cybr-btn__tag">BATNA05</span>
-                </button>
-            </div>
+                    <FaGithub />  Github
+                </Button>
 
-            <div className='cybr-btn-container'>
-                <button class="cybr-btn" onClick={() => {
+                <Button class="cybr-btn" onClick={() => {
                     window.location.href='https://twitter.com/saphidev'
                 }}>
-                    Twitter<span aria-hidden>_</span>
-                    <span aria-hidden class="cybr-btn__glitch">Apotox_</span>
-                    <span aria-hidden class="cybr-btn__tag">BATNA05</span>
-                </button>
-            </div>
+                   <FaTwitter /> Twitter
+                </Button>
+          
 
 
-            <div className='cybr-btn-container'>
-                <button class="cybr-btn" onClick={() => {
+            
+                <Button class="cybr-btn" onClick={() => {
                     history.push('/how-it-works')
                 }}>
                     How it Works<span aria-hidden>_</span>
                     <span aria-hidden class="cybr-btn__glitch">Apotox_</span>
                     <span aria-hidden class="cybr-btn__tag">BATNA05</span>
-                </button>
-            </div>
+                </Button>
+           
 
 
         </div>
